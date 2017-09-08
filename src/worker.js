5ceb4a7a3e26e103c3917719a675bb22560bb0ac
Markus Felten <Markus.Felten@gmx.de>
2017-09-08 21:38:03 +0200

    fix: leveldb writing

--- src/worker.js
+++ src/worker.js
@@ -1,9 +1,18 @@
-import sqlite from 'sqlite';
-
-export async function sqlite2leveldb(sqldb) {
+export async function sqlite2leveldb(sqldb, leveldb) {
   const stmt = await sqldb.prepare('SELECT date,type,amount FROM value_date');
 
   stmt.each((err, result) => {
     console.log(result);
+
+    const key = `${result.type}/${result.date}`;
+
+    leveldb.put(key, result.amount, err => {
+      if (err) return console.log('Ooops!', err);
+
+      leveldb.get(key, (err, value) => {
+        if (err) return console.log('Ooops!', err);
+        console.log(`${key}=${value}`);
+      });
+    });
   });
 }
--- tests/migrate-test.js
+++ tests/migrate-test.js
@@ -2,14 +2,17 @@ import test from 'ava';
 import { sqlite2leveldb } from '../src/worker';
 import sqlite from 'sqlite';
 
+const levelup = require('levelup');
 const path = require('path');
 
 test('migrate', async t => {
-  const db = await sqlite.open(
+  const leveldb = levelup(path.join(__dirname, '../build', 'leveldb'));
+
+  const sqldb = await sqlite.open(
     path.join(__dirname, '../tests', 'fixtures', 'sample.db')
   );
 
-  await sqlite2leveldb(db);
+  await sqlite2leveldb(sqldb, leveldb);
 
   t.pass();
 });
