ed63850943468641be9b8b936d8ee7adc6612e92
Markus Felten <Markus.Felten@gmx.de>
2019-01-14 16:49:41 +0100

    test: mkdir ...build

--- tests/migrate-test.js
+++ tests/migrate-test.js
@@ -1,15 +1,18 @@
-import test from 'ava';
-import { sqlite2leveldb } from '../src/worker';
-import sqlite from 'sqlite';
-import { join } from 'path';
-import levelup from 'levelup';
-import leveldown from 'leveldown';
+import test from "ava";
+import { sqlite2leveldb } from "../src/worker";
+import sqlite from "sqlite";
+import { join } from "path";
+import levelup from "levelup";
+import leveldown from "leveldown";
+import fs from "fs";
 
-test.serial('migrate', async t => {
-  const leveldb = levelup(leveldown(join(__dirname, '..', 'build', 'leveldb')));
+test.serial("migrate", async t => {
+  await fs.promises.mkdir(join(__dirname, "..", "build"), { recursive: true });
+
+  const leveldb = levelup(leveldown(join(__dirname, "..", "build", "leveldb")));
 
   const sqldb = await sqlite.open(
-    join(__dirname, '..', 'tests', 'fixtures', 'sample.db')
+    join(__dirname, "..", "tests", "fixtures", "sample.db")
   );
 
   await sqlite2leveldb(sqldb, leveldb);
@@ -18,24 +21,24 @@ test.serial('migrate', async t => {
   t.pass();
 });
 
-test.cb('list', t => {
+test.cb("list", t => {
   t.plan(1);
 
-  const leveldb = levelup(leveldown(join(__dirname, '..', 'build', 'leveldb')));
+  const leveldb = levelup(leveldown(join(__dirname, "..", "build", "leveldb")));
 
-  const readStream = leveldb.createReadStream({ start: 'pv/0', end: 'pv/Z' });
+  const readStream = leveldb.createReadStream({ start: "pv/0", end: "pv/Z" });
 
-  readStream.on('data', data => {
-    //console.log(data.key + ' = ' + data.value);
+  readStream.on("data", data => {
+    console.log(data.key + " = " + data.value);
 
-    if (data.key.toString() === 'pv/1030665600' && data.value == 2004.1) {
-      console.log(data.key + ' = ' + data.value);
+    if (data.key.toString() === "pv/1030665600" && data.value == 2004.1) {
+      console.log(data.key + " = " + data.value);
 
       t.pass();
     }
   });
 
-  readStream.on('end', () => {
+  readStream.on("end", () => {
     leveldb.close();
     t.end();
   });
