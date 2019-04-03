29d7f54f4984d1df979a5a7438aef2fd0cdce562
Markus Felten <Markus.Felten@gmx.de>
2019-04-04 00:20:12 +0200

    fix: also migrate content of value_type

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,5 +1,4 @@
-
-import { Category } from 'konsum-db';
+import { Category } from "konsum-db";
 
 /**
  * Convert sqlite data into leveldb data
@@ -13,26 +12,45 @@ export async function sqlite2leveldb(sqldb, leveldb) {
   const categories = new Map();
 
   for await (const c of Category.entries(leveldb)) {
-    categories.set(c.name,c);
+    categories.set(c.name, c);
   }
+
+  await sqldb.each(
+    "SELECT id,name,unit,color,decimal_places FROM value_type",
+    (err, result) => {
+      let c = categories.get(result.id);
+      if (c === undefined) {
+        c = new Category(result.id, {
+          description: result.description,
+          unit: result.unit,
+          color: result.result,
+          decimal_places: result.decimal_places
+        });
+        c.write(leveldb);
+        categories.set(c.name, c);
+        //console.log(c, result);
+      }
+    }
+  );
 
   let n = 0;
 
   await sqldb.each(
     `SELECT ${DATE},type,amount FROM value_date`,
     (err, result) => {
-      console.log(n,result);
+      //console.log(n, result);
       let c = categories.get(result.type);
-      if(c === undefined) {
+      if (c === undefined) {
         c = new Category(result.type, {});
         c.write(leveldb);
-        categories.set(c.name,c);
+        categories.set(c.name, c);
+        console.log(`unknown category ${result.type}`);
       }
-      c.writeValue(leveldb,parseFloat(result[DATE]),result.amount);
+      c.writeValue(leveldb, parseFloat(result[DATE]), result.amount);
       n++;
     }
   );
 
-  console.log("NNNN",n);
+  console.log("NNNN", n);
   return n;
 }
