457e37849724fe85e014efac3d1bd388b1a5c83b
Markus Felten <Markus.Felten@gmx.de>
2020-07-17 12:34:16 +0200

    fix: remove unused vars

--- src/timeseries-sqlite2leveldb-cli.mjs
+++ src/timeseries-sqlite2leveldb-cli.mjs
@@ -8,7 +8,7 @@ async function migrate() {
   console.log(`migrate ${process.argv[2]} -> ${process.argv[3]}`);
   const sqldb = await sqlite.open(process.argv[2]);
   const leveldb = await levelup(leveldown(process.argv[3]));
-  const master = await initialize(leveldb);
+  await initialize(leveldb);
 
   const n = await sqlite2leveldb(sqldb, leveldb);
   console.log(`${n} records migrated`);
